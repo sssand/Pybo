2cdd71296caba36bb2a4eb3432df54c52c508702
sssand <koominjae9@naver.com>
2022-01-06 23:21:38 +0900

    ip#4

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ["192.168.75.161", "sssand.mcv.kr"]
+ALLOWED_HOSTS = ["192.168.45.56", "sssand.mcv.kr"]
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
